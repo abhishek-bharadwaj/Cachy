2060b13e5ff555fff7725b3fed63b5a1d52b133e
Abhishek Bharadwaj <abhishek.bharadwaj@healthify.co>
2018-12-11 12:19:35 +0530

    Made method

--- app/src/main/java/com/abhishek/cachy/MainActivity.kt
+++ app/src/main/java/com/abhishek/cachy/MainActivity.kt
@@ -2,11 +2,6 @@ package com.abhishek.cachy
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import io.reactivex.Single
-import io.reactivex.SingleObserver
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -20,50 +15,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        pb.visible()
         GlobalScope.launch(Dispatchers.Main) {
-            val response = Api.apiService.getAllMovies().await()
-            val movieData = response.body()
+            val movies = getMoviesData()
             pb.gone()
             tv.visible()
-            if (response.isSuccessful && movieData?.isNotEmpty() == true) {
-                movieData.forEach {
-                    tv.append("${it.title}\n")
-                }
-            } else {
-                tv.text = "Api call failed ${response.code()}"
-            }
+            movies?.forEach { tv.append(it.title) }
         }
-        /*mockApiCall(object : ResultListener<String> {
-            override fun onSuccess(t: String) {
-                Log.v(TAG, t)
-            }
-
-            override fun onFailure(e: Throwable) {
-                Log.e(TAG, e.message)
-            }
-        })*/
     }
 
-    private fun mockApiCall(resultListener: ResultListener<String>) {
-        Single.defer {
-            Single.just(getData())
-        }.subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : SingleObserver<String> {
-                override fun onSuccess(t: String) {
-                    resultListener.onSuccess(t)
-                }
-
-                override fun onSubscribe(d: Disposable) {
-
-                }
-
-                override fun onError(e: Throwable) {
-                    resultListener.onFailure(e)
-                }
-            })
+    private suspend fun getMoviesData(): List<MovieData>? {
+        val response = Api.apiService.getAllMovies().await()
+        val movieData = response.body()
+        return if (response.isSuccessful && movieData?.isNotEmpty() == true) {
+            movieData
+        } else {
+            null
+        }
     }
 
     private fun getData(): String {
@@ -71,13 +38,8 @@ class MainActivity : AppCompatActivity() {
         if (key != null && cacheRepository.isDataPresent(key)) {
             return cacheRepository.getData(key, String::class.java)
         }
-        val data = someLongRunningTask()
+        val data = ""
         key?.let { cacheRepository.saveData(it, data) }
         return data
-    }
-
-    private fun someLongRunningTask(): String {
-        Thread.sleep(4000)
-        return "Hello.."
     }
 }
--- app/src/main/java/com/abhishek/cachy/ResultListener.kt
+++
@@ -1,7 +0,0 @@
-package com.abhishek.cachy
-
-interface ResultListener<T> {
-    fun onSuccess(t: T)
-
-    fun onFailure(e: Throwable)
-}
